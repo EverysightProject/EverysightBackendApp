4ec5723eb4aa0594f2af0791c3ae54e0ae906aca
ArielY15 <ariely15@gmail.com>
2017-04-16 14:43:11 +0300

    added maps library

--- backend/src/main/java/everysight/backend/MyServlet.java
+++ backend/src/main/java/everysight/backend/MyServlet.java
@@ -6,7 +6,16 @@
 
 package everysight.backend;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.maps.DirectionsApi;
+import com.google.maps.GaeRequestHandler;
+import com.google.maps.GeoApiContext;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.EncodedPolyline;
+
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import javax.servlet.http.*;
 
@@ -16,6 +25,22 @@ public class MyServlet extends HttpServlet {
             throws IOException {
         resp.setContentType("text/plain");
         resp.getWriter().println("Please use the form to POST to this url");
+        try {
+            GeoApiContext context = new GeoApiContext(new GaeRequestHandler()).setApiKey("AIzaSyBnZO0bEmFgi3XN64zeafdAQSurfdfe4F8");
+            //GeoApiContext context = new GeoApiContext().setApiKey("AIzaSyBnZO0bEmFgi3XN64zeafdAQSurfdfe4F8");
+
+            DirectionsResult result = DirectionsApi.getDirections(context, "Sydney, AU",
+                    "Melbourne, AU").await();
+            //GeocodingResult[] results = GeocodingApi.geocode(context,"1600 Amphitheatre Parkway Mountain View, CA 94043").await();
+            for (int i=0;i<result.routes.length;i++ ) {
+                //for (int j=0;j<result.routes[i].legs.length;j++)
+                    resp.getWriter().println(result.routes[i].summary);
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.toString());
+        }
     }
 
     @Override
