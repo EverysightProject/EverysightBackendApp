ad0505d15e4d99189c892db6c3c2f078e1881879
ArielY15 <ariely15@gmail.com>
2017-11-14 20:26:30 +0200

    places - WORKING

--- backend/src/main/java/everysight/backend/PlacesAPI/PlacesRequest.java
+++ backend/src/main/java/everysight/backend/PlacesAPI/PlacesRequest.java
@@ -2,16 +2,18 @@ package everysight.backend.PlacesAPI;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
 import com.google.maps.GaeRequestHandler;
 import com.google.maps.GeoApiContext;
 import com.google.maps.NearbySearchRequest;
 import com.google.maps.PlacesApi;
 import com.google.maps.internal.LatLngAdapter;
-import com.google.maps.model.PlaceType;
 import com.google.maps.model.PlacesSearchResponse;
 import com.google.maps.model.LatLng;
+import com.google.maps.model.PlacesSearchResult;
 
 import org.apache.commons.io.IOUtils;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -27,7 +29,9 @@ public class PlacesRequest {
 
             String parameters = IOUtils.toString(req.getInputStream(), "UTF-8");
 
-            Gson gson = new GsonBuilder().registerTypeAdapter(LatLng.class, new LatLngAdapter()).create();
+            Gson gson = new GsonBuilder()
+                    .registerTypeAdapter(LatLng.class, new LatLngAdapter())
+                    .create();
             NearByParameters nearByParameters = gson.fromJson(parameters,NearByParameters.class);
             resp.setContentType("application/json");
 
@@ -36,7 +40,15 @@ public class PlacesRequest {
 
             PlacesSearchResponse response = nearbySearchRequest.await();
 
-            resp.getWriter().println(gson.toJson(response));
+            JsonArray places = new JsonArray();
+            for(PlacesSearchResult res : response.results)
+            {
+                MyPlace p = new MyPlace();
+                p.name = res.name;
+                p.location = res.geometry.location;
+                places.add(gson.toJson(p));
+            }
+            resp.getWriter().println(places);
         }
         catch(Exception e)
         {
